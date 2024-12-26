a9fdcdd7115b071a814fe61d9b2695db218b7b4a
Jorge Altamirano <alojorge13@gmail.com>
2024-12-26 19:03:46 -0300

    second commit

--- src/main.js
+++ src/main.js
@@ -1,24 +1,131 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const Containerproductos = document.querySelector(".container-productos");
+let cont = 0
+let confirm = false
+const Agregarproductos = async () => {
+   
+    try {
+        const response = await fetch('./data.json');
+        const products = await response.json();
 
-setupCounter(document.querySelector('#counter'))
+        renderProducts(products);
+    } catch (error) {
+        console.error('error al cargar los productos', error);
+    }
+}
+
+const renderProducts = (products) => {
+    console.log(cont++)
+    products.forEach((product, index) => {
+        const productElement = document.createElement("div");
+        productElement.classList.add("product");
+        productElement.innerHTML = `
+        <div class="product__container-img">
+            <img class="product-img" src="${product.image.thumbnail}" alt="${product.name}">
+            <div class="container-button">
+                <button class="add-to-cart" id="${index}">
+                    <div class="product__container-icon">
+                        <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20">
+                            <g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/>
+                            <path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/>
+                            </g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs>
+                        </svg> 
+                    </div>
+                        Add to Cart
+                </button>           
+            </div>        
+        </div>
+        
+         <p>${product.category}</p>
+         <h3>${product.name}</h3>
+         <p class="price">$${product.price.toFixed(2)}</p>
+        
+        `
+        Containerproductos.appendChild(productElement);
+    });
+    handleAddToCart()
+}
+
+const handleIncrementDecrement = (button) => {
+    const incrementButton = button.querySelector(".increment");
+    const decrementButton = button.querySelector(".decrement");
+    const quantilyElement = button.querySelector(".quantily");
+
+    if (incrementButton) {
+        incrementButton.addEventListener("click", () => {
+            let quantity = parseInt(quantilyElement.textContent, 10);
+            quantilyElement.textContent = quantity + 1; // Incrementa la cantidad
+        });
+    }
+
+    if (decrementButton) {
+        decrementButton.addEventListener("click", () => {
+            let quantity = parseInt(quantilyElement.textContent, 10);
+            if (quantity > 1) {
+                quantilyElement.textContent = quantity - 1; // Decrementa la cantidad
+            } else if (quantity === 1) {
+                // Removemos la clase `added` para indicar que no está en el carrito
+                button.classList.remove("added");
+
+                // Actualizamos el contenido del botón al estado inicial
+                const iconHtml = `
+                    <div class="product__container-icon">
+                        <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20">
+                            <g fill="#C73B0F" clip-path="url(#a)">
+                                <path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/>
+                                <path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/>
+                            </g>
+                            <defs>
+                                <clipPath id="a">
+                                    <path fill="#fff" d="M.333 0h20v20h-20z"/>
+                                </clipPath>
+                            </defs>
+                        </svg> 
+                    </div>
+                    Add to Cart
+                `;
+                button.innerHTML = iconHtml;
+
+                // Eliminamos los eventos para evitar que se reinicialicen
+                button.onclick = null;
+            }
+        });
+    }
+};
+
+const handleAddToCart = () => {
+    const buttons = document.querySelectorAll(".add-to-cart");
+
+    buttons.forEach((button) => {
+        // Aseguramos que no haya duplicados de eventos
+        button.onclick = null;
+
+        button.addEventListener("click", () => {
+            if (!button.classList.contains("added")) {
+                button.classList.add("added");
+
+                // Actualizamos el contenido del botón
+                const addedHtml = `
+                    <div class="product__container-icon">
+                        <svg class="decrement" xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2">
+                            <path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/>
+                        </svg>
+                        <p class="quantily">1</p>
+                        <svg class="increment" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10">
+                            <path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/>
+                        </svg>
+                    </div>
+                `;
+                button.innerHTML = addedHtml;
+
+                // Asociamos los eventos al nuevo contenido
+                handleIncrementDecrement(button);
+            }
+        });
+    });
+};
+
+
+
+// Llamada inicial para cargar los productos
+Agregarproductos();
